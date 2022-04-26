0be1540ac6bed6a909b584370a5809ab9c833a2f
Jose Castro <jacastro023@gmail.com>
2022-04-26 17:54:15 -0500

    added feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -20,3 +20,30 @@ class Toy(models.Model):
 
   def __str__(self):
         return f"The toy named {self.name} has a color of {self.color} and an id of {self.id}"
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        #choices
+        choices= MEALS,
+        default=MEALS[0][0]
+    )
+
+    #  the foreign key always goes on the many side
+    #internally it will be cat_id the _id automatically gets added
+    fish = models.ForeignKey(Fish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        #this method will give us friendly field choices value (breakfast instead of B)
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering: ['-date']
